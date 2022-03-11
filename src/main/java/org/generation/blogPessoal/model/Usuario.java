8836a7a9f9b91946065940673c0e333804e73503
sotie <javacursobustos@gmail.com>
2022-03-11 19:02:55 -0300

    @bustos00 [Task:ConfigJunit, swagger e deploy]

--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -5,8 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -19,15 +22,32 @@ public class Usuario {
 	@NotNull
 	@Size(min = 2, max =100)
 	private String nome;
+
 	
-	@NotNull
-	@Size(min = 5, max =100)
+	@Size(max = 5000)
+	private String foto;
+	
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+
 	private String usuario;
 	
 	@NotNull
 	@Size(min = 5, max =100)
 	private String senha;
+	
+	
 
+	public Usuario(long id, String nome, String foto, String usuario,  String senha) { 
+		this.id = id;
+		this.nome = nome;
+		this.foto = foto;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	public Usuario() { }
+ 
 	public long getId() {
 		return id;
 	}
@@ -42,6 +62,13 @@ public class Usuario {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 	public String getUsuario() {
